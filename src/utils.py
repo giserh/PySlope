0990bdcc665e5be4d6ac87a20a2c694753aa86bd
Duan Uys <duan_uys@icloud.com>
2016-07-27 11:51:33 +1000

    code fix

--- src/utils.py
+++ src/utils.py
@@ -105,7 +105,7 @@ def formatCircleData(coordinates):
 
     for element in coordinates.split():
         results.append(element)
-
+    print len(results)
     if len(results) > 4:
         raiseGeneralError("There are too many data points for your ellipse. Check config file")
     elif len(results) <= 3:
