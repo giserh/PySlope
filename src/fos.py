b63bcba3b1b1b68fb882f1af58dee3181a131959
Duan Uys <duan_uys@icloud.com>
2016-07-28 13:29:12 +1000

    added percent success to calcluation

--- src/fos.py
+++ src/fos.py
@@ -108,7 +108,8 @@ def FOS_Method( method,
     # convert calculated lists into numpy arrays
     numerator_list, denominator_list = np.array(numerator_list), np.array(denominator_list)
     print errors, slice
-    success = float(100. * (1. - (errors/slice)))
+    frac = float(errors) / float(slice)
+    success = 100 - frac
     error_result =  "\nTotal number of errors encountered: %s\nPercent Success: %f%%" % (str(errors), (success))
 
     # calculate actual FOS from lists
