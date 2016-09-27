72a6a96a4dbb28c1cab84eefd1d8279d607decfb
Duan Uys <duan_uys@icloud.com>
2016-09-27 11:22:19 +1000

    various commits

--- utils/create.py
+++ utils/create.py
@@ -31,20 +31,12 @@ def shapely_line(config, profile_data):
 	return LineString(profile_data)
 	
 def shapely_circle(config):
-	if len(config.ellipse_coorindates) < 4:
-		exit("Ellipse Coordinates are not formatted correctly."
-		     " '{}'".format(config.ellipse_coordinates))
-	
-	print len(config.ellipse_coorindates), config.ellipse_coorindates
-	exit()
 	v = config.verbose
 	x = config.ellipse_coordinates[0]
 	y = config.ellipse_coordinates[1]
 	a = config.ellipse_coordinates[2]
 	b = config.ellipse_coordinates[3]
 	
-	if len(config.ellipse_coordinates) == 5:
-		pass
 	g.verb(v, "Creating Shapely circle with ellipsoid data: {},{},{},{}."
 	          "".format(x,y,a,b))
 	
@@ -55,6 +47,10 @@ def shapely_circle(config):
 def ellipse(config):
 	def _deg2rad(degree):
 		return degree * np.pi / 180.
+	
+	if len(config.ellipse_coorindates) < 4:
+		exit("Ellipse Coordinates are not formatted correctly."
+		     " '{}'".format(config.ellipse_coordinates))
 	
 	v = config.verbose
 	x = config.ellipse_coordinates[0]
